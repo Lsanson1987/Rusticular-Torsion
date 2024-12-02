bea9592bd985e674f832a64f0fd896e9a6511f3f
Sirlizard <jpdyser2005@gmail.com>
2024-12-01 21:10:10 -0600

    Created the class to will need to be filled out will need dynamic additons for front end

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod monsters;
+
 use std::error::Error;
 
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main()-> Result<(), Box<dyn Error>>{
+
     let ui = AppWindow::new()?;
 
     ui.on_request_increase_value({
--- src/monsters.rs
+++ src/monsters.rs
@@ -1,1 +1,54 @@
 
+
+
+pub(crate) struct Monster {
+    name: String,
+    initiative: u8,
+    hit_points: u8,
+    armor_class: u8,
+    notes: String,
+}
+
+impl Monster {
+    pub fn new(name: &str, initiative: u8, hit_points: u8, armor_class: u8, notes: &str) -> Monster {
+      Monster {  name: name.to_string(),
+        initiative,
+        hit_points,
+        armor_class,
+        notes: notes.to_string(),
+      }
+    }
+    pub fn display_name(&self) {
+        println!("{}", self.name);
+    }
+    pub fn display_initiative(&self) {
+        println!("{}", self.initiative);
+    }
+    pub fn display_hit_points(&self) {
+        println!("{}", self.hit_points);
+    }
+    pub fn display_armor_class(&self) {
+        println!("{}", self.armor_class);
+    }
+    pub fn display_notes(&self) {
+        println!("{}", self.notes);
+    }
+    pub fn change_initiative(&mut self, int: u8) {
+        self.initiative = int;
+    }
+    pub fn change_armor(&mut self, int: u8) {
+        self.armor_class = int;
+    }
+    pub fn change_name(&mut self, name: &str) {
+        self.name = name.to_string();
+    }
+    pub fn change_note(&mut self, name: &str) {
+        self.notes = name.to_string();
+    }
+    pub fn damage(&mut self, x: u8) {
+        self.hit_points = self.hit_points - x;
+    }
+    pub fn heal(&mut self, x: u8) {
+        self.hit_points = self.hit_points + x;
+    }
+}
