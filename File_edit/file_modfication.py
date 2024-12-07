faebf732e7b188288df20f656414c8463a258889
Sirlizard <jpdyser2005@gmail.com>
2024-12-06 18:22:05 -0600

    Monsters Data Bank is completed

--- File_edit/file_modfication.py
+++ File_edit/file_modfication.py
@@ -2,22 +2,18 @@ import csv
 
 ##need to fix soon possible error with file name ?????/
 with open("correct_monster_data.csv", 'w') as csvfile:
-    fields = ['id', 'ruleset', 'name', 'AC', 'HP', 'Dex', 'HDP']
+    fields = ['Name', 'AC', 'HP', 'Int', 'Notes']
     writer = csv.DictWriter(csvfile, fieldnames=fields) 
     writer.writeheader()
-    with open("Monster_Data.txt", 'r') as readfile : 
-        ##issue with this
-        lines = readfile.readline()
-        for line in lines:
-            ##remove last },
+    writer.writerow({'Name' : 'a', 'AC' : 'a', 'HP' : 'a', 'Int' : 'a', 'Notes' : 'a'})
+    with open("Monster_Data.txt", encoding="utf-8") as f:
+        for line in f:
             clean_line = line.strip()[1:-2]
-            ##fix this up
-            clean_line_list = clean_line.find(", ")
-            data_row = []
-            ##fix this as well small stuff
-            for var in clean_line_list :
-                index = var.index(":")
-                value = var[index + 1:].strip().replace("'", ' ').replace('"', ' ')
-                data_row.append(value)
-            writer.writerow({'id' : data_row[0], 'ruleset' : list[1], 'name' : list[2], 'AC' : list[3], 'HP' : list[3], 'Dex' : list[4], 'HDP' : list[5]})
+            array = clean_line.split(", ")
+            name = array[2][7:-1]
+            ac = array[3][4:]
+            hp = array[4][4:]
+            dex = array[5][5:]
+            print(dex)
+            writer.writerow({'Name' : name, 'AC' : ac, 'HP' : hp, 'Int' : dex, 'Notes' : ''})
 
