6d692588c2a1be3739de9e33414a51a28010c654
Sirlizard <jpdyser2005@gmail.com>
2024-12-08 13:49:49 -0600

    I have created a map with moster name and Monster to be able to push back

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,20 @@
 // mod mapped;
 //mod monsters;
 //use monsters::Monster;
-use wasm_bindgen::prelude::*; 
+use std::{collections::HashMap, hash::Hash, iter};
+//use wasm_bindgen::prelude::*; 
 use rand::prelude::*;
+use std::collections::{hash_map, HashMap};
 use std::io::{self, BufReader};
 
 
 slint::include_modules!();
 
 use std::rc::Rc;
-use csv::Reader;
+//use csv::Reader;
 // use std::error::Error;
-// use std::fs::File;
-// use std::path::Path;
+use std::fs::File;
+use std::path::Path;
 
 
 use slint::{Model, ModelExt, ModelRc, SharedString, StandardListViewItem, VecModel};
@@ -27,14 +29,10 @@ use slint::{Model, ModelExt, ModelRc, SharedString, StandardListViewItem, VecMod
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
     //file data base read 
-    //let mut monster_list:Vec<monsters::Monster>  = Vec::new();
-    // let file_path = "monsters.csv";
-    // let mut reader = Reader::from_path(file_path)?;
-
-    // for result in reader.records() {
-    //     let record = result?;
-    //     println!("{:?}", record);
-    // }
+    let monster_list: HashMap<String, Monster>  = monster_data_base();
+    for (name, monster) in monster_list {
+        println!("this is the name {}", name);
+    }
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(all(debug_assertions, target_arch = "wasm32"))]
@@ -49,11 +47,11 @@ pub fn main() {
         let items: Rc<VecModel<StandardListViewItem>> = Rc::new(VecModel::default());
 
         for c in 1..5 { // columns
-            if c == 1 {
+            if c == 2 {
                 //let initiative = rand::random::<u8>() % 100;
                 let input_num = input_request(0);
                 items.push(slint::format!("{input_num}").into());
-            } else if c == 2 {
+            } else if c == 1 {
                 println!("Please enter your Name:");
                 let mut name = String::new();
                 io::stdin()
@@ -61,6 +59,9 @@ pub fn main() {
                     .expect("Failed to read line");
                 println!("You enterd an name {}", name.trim());
                 items.push(slint::format!("{name}").into());
+                // for (name, monster) in monster_list {
+                //     println!("this is the name {}", name);
+                // }
             } else if c == 3 {
                 //let ac = 10 + r * 2;
                 let input_num = input_request(1);
@@ -127,4 +128,108 @@ fn filter_sort_model(
     }
 
     model
+}
+
+fn monster_data_base() -> HashMap<String, Monster> {
+    let mut monster_list: HashMap<String, Monster> = HashMap::new();
+    let file_path = "monsters.csv";
+    let file = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+            return monster_list; // return an empty map in case of error
+        }
+    };
+    
+    let reader = io::BufReader::new(file);
+    let mut first_line = true;
+
+    for line in io::BufRead::lines(reader) {
+        match line {
+            Ok(line) => {
+                if first_line {
+                    first_line = false;
+                    continue;
+                }
+                let values: Vec<String> = line.split(',')
+                                              .map(|s| s.to_string())
+                                              .collect();
+
+                let ac: u64 = values[1].parse().unwrap_or(0);
+                let hp: u64 = values[2].parse().unwrap_or(0);
+                let dwa: u64 = values[3].parse().unwrap_or(0);
+                let monster = Monster::new(&values[0], ac, hp, dwa, "");
+                monster_list.insert(values[0].clone(), monster);
+            },
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+            }
+        }
+    }
+
+    monster_list
+}
+
+fn check_if_name(monster_list: &HashMap<String, Monster>, name: String) -> bool {
+    return monster_list.contains_key(&name);
+}
+pub struct Monster {
+    name: String,
+    initiative: u64,
+    hit_points: u64,
+    armor_class: u64,
+    notes: String,
+}
+
+impl Monster {
+    pub fn new(name: &str, initiative: u64, hit_points: u64, armor_class: u64, notes: &str) -> Monster {
+      Monster {  name: name.to_string(),
+        initiative,
+        hit_points,
+        armor_class,
+        notes: notes.to_string(),
+      }
+    }   
+    pub fn copy(&self) -> Monster {
+        Monster {
+            name: self.name.clone(),
+            initiative: self.initiative,
+            hit_points: self.hit_points,
+            armor_class: self.armor_class,
+            notes: self.notes.clone(),
+        }
+    }
+    pub fn display_name(&self) {
+        println!("{}", self.name);
+    }
+    pub fn display_initiative(&self) {
+        println!("{}", self.initiative);
+    }
+    pub fn display_hit_points(&self) {
+        println!("{}", self.hit_points);
+    }
+    pub fn display_armor_class(&self) {
+        println!("{}", self.armor_class);
+    }
+    pub fn display_notes(&self) {
+        println!("{}", self.notes);
+    }
+    pub fn change_initiative(&mut self, int: u64) {
+        self.initiative = int;
+    }
+    pub fn change_armor(&mut self, int: u64) {
+        self.armor_class = int;
+    }
+    pub fn change_name(&mut self, name: &str) {
+        self.name = name.to_string();
+    }
+    pub fn change_note(&mut self, name: &str) {
+        self.notes = name.to_string();
+    }
+    pub fn damage(&mut self, x: u64) {
+        self.hit_points = self.hit_points - x;
+    }
+    pub fn heal(&mut self, x: u64) {
+        self.hit_points = self.hit_points + x;
+    }
 }
